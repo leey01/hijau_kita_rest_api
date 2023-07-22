532d032b1a4cdcc3a050e54832bbb6d65e5c040f
eicheich <nengindah41@gmail.com>
2023-07-22 18:46:46 +0700

    model trx event

--- app/Models/TrxEvent.php
+++ app/Models/TrxEvent.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class TrxEvent extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'description',
+        'point_earned',
+        'created_at',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class)->with('sub_category');
+    }
+
+
 }
