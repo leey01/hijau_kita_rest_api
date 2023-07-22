156c4dbe4319d5098eb2fa73b233a542ec8240d4
Sejati Adli <sejatiadli@gmail.com>
2023-07-22 19:51:02 +0700

    [SA] fix model susdev_goals

--- app/Models/SusdevGoals.php
+++ app/Models/SusdevGoals.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class SusdevGoals extends Model
 {
     use HasFactory;
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'pivot',
+        'id',
+        'image'
+    ];
+
+    protected $appends = [
+        'image_url',
+    ];
+
+    public function getImageUrlAttribute()
+    {
+        if ($this->image == null) {
+            return null;
+        }
+        return Storage::disk('public')->url($this->image);
+    }
 }
